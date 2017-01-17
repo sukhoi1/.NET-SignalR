2c71ae505a80cc5103cfefd7d94ffec1dbe1f5c5
Anton Lyhin <Anton_Lyhin@epam.com>
2017-01-17 17:12:11 +0300

    v0.003 - chrome SignalR console-chat;

---
+++ SignalRChat/DemoHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace SignalRChat
+{
+    public class DemoHub : Hub
+    {
+        public void Post(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.demoMessage(name, message);
+        }
+    }
+}
